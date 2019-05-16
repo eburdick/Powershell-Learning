62fff7a27c0c3105b20239c6a55d91dff2bd06ea
edburdick <ed@edburdick.net>
2019-05-15 22:03:34 -0400

    sync checkin

--- play01.ps1
+++ play01.ps1
@@ -1,11 +1,19 @@
 #
 # playing with powershell
 #
-# get current directory
-$dir = Get-ChildItem
-#$dir
+# get current directory contents
+$dircontents = Get-ChildItem -Path '.' #note:  -Path '.' is optional because it is the default
+#$dircontents
 # display entries in current directory
-foreach ($item in $dir)
+foreach ($item in $dircontents)
 {
     $item
 }
+# display parent directory
+$parent = (Get-Item . ).Parent
+echo "parent"
+$parent
+
+# display sibling directories
+$sibs = Get-ChildItem $parent
+$sibs
