a911dc63f74e1b10185d92cd07932a09e7a4bf1d
edburdick <ed@edburdick.net>
2019-05-17 16:18:49 -0400

    Sync checkin

--- play01.ps1
+++ play01.ps1
@@ -1,19 +1,29 @@
 #
 # playing with powershell
 #
-# get current directory contents
-$dircontents = Get-ChildItem -Path '.' #note:  -Path '.' is optional because it is the default
+
+#Get current directory. This is a directory object, not a string. To display its full name,
+#we just write the name of the directory variable with the FullName field specified
+$current_directory = Get-Item . ;
+$current_directory.FullName;
+$current_directory.GetType();
+
+# get current directory contents.  This is a list of its contents
+$dircontents = Get-ChildItem $current_directory; #note:  -Path '.' is optional because it is the default
 #$dircontents
 # display entries in current directory
 foreach ($item in $dircontents)
 {
-    $item
+    $item.Name;
 }
 # display parent directory
-$parent = (Get-Item . ).Parent
-Write-Output "parent"
-$parent.FullName
-$parent.GetDirectories()
+$parent = (Get-Item . ).Parent;
+Write-output "";
+Write-Output parent;
+$parent.FullName;
+
+Write-Output "";
+$parent.GetDirectories();
 
 # display sibling directories
 #$sibs = Get-ChildItem $parent
